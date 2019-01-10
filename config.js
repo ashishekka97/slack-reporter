293343e01577b5f8771d947a411fc8ae379da648
Ashish Ekka <ashishekka329@gmail.com>
2019-01-11 00:35:04 +0530

    config changed

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     database: process.env.DB_NAME || 'sys',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '',
-    host: process.env.HOST || 'localhost'
+    host: process.env.DB_URL || 'localhost'
   },
   slack: {
     url: 'https://hooks.slack.com/services/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'
